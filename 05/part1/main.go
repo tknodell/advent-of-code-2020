b25ec5a0b1d94f0d9c723b18c8de7b8c88c014cf
tknodell <tknodell@users.noreply.github.com>
2020-12-04 23:49:29 -0700

    rows working

--- 05/part1/main.go
+++ 05/part1/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+const TotalRows = 128
+
 func readLines(path string) ([]string, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -25,10 +27,34 @@ func main() {
 	values, _ := readLines("input.txt")
 	for _, v := range values {
 		rowValue := v[0:7]
-		fmt.Println(calcRow(rowValue))
+		calcRow(rowValue)
 	}
+
+	calcRow("FBFBBFFRLR")
+}
+
+func popRows() []int {
+	var r []int
+	for i := 0; i < TotalRows; i++ {
+		r = append(r, i)
+	}
+	return r
 }
 
 func calcRow(s string) int {
-	return -1
+	rows := popRows()
+
+	for _, v := range s {
+		letter := string(v)
+		fmt.Println(letter)
+		switch letter {
+		case "F":
+			rows = rows[0 : len(rows)/2]
+		case "B":
+			rows = rows[len(rows)/2:]
+		}
+	}
+
+	fmt.Println(rows)
+	return len(rows)
 }
