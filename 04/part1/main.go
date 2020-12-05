29a192687a7ab26ff0f94947292793f090c22653
tknodell <tknodell@users.noreply.github.com>
2020-12-04 21:45:17 -0700

    start to day 4

---
+++ 04/part1/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var requiredFields = []string{
+	"byr",
+	"iyr",
+	"eyr",
+	"hgt",
+	"hcl",
+	"ecl",
+	"pid",
+	// "cid",
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func main() {
+
+	var validPassport int
+	lines, _ := readLines("input.txt")
+	var Passport string
+
+	for _, v := range lines {
+
+		if v == "" {
+			// hit blank line
+
+			if hasValidFields(Passport, requiredFields) {
+				fmt.Println("passport is valid")
+				fmt.Println(Passport)
+				validPassport++
+			}
+			fmt.Println("new passport!\n")
+			Passport = ""
+		} else {
+			Passport += v
+		}
+	}
+
+	fmt.Println(validPassport, "valid passports")
+}
+
+func hasValidFields(p string, fields []string) bool {
+	for _, v := range fields {
+		if !strings.Contains(p, v) {
+			return false
+		}
+	}
+	return true
+}
+
+// 234 too low
