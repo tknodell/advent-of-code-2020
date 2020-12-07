edbea0a46fbe7fba4edbbcf52ca55535ae079158
tknodell <tknodell@users.noreply.github.com>
2020-12-07 13:41:05 -0700

    6 part1 done

--- 06/part1/main.go
+++ 06/part1/main.go
@@ -24,14 +24,40 @@ func readLines(path string) ([]string, error) {
 func main() {
 	res, _ := readLines("input.txt")
 	var group string
+	var countSum int
 	for _, v := range res {
 		if v != "" {
-			fmt.Println("adding to group", v)
+			// fmt.Println("adding to group", v)
 			group += v
 		} else {
 			fmt.Println("new group; result is", group)
+			countSum += uniqLetters(group)
 			group = ""
 		}
 		fmt.Println(v)
 	}
+	fmt.Println("result is ", countSum)
+}
+
+func uniqLetters(s string) int {
+	var letters []string
+
+	for _, v := range s {
+		value := string(v)
+		if !contains(letters, value) {
+			letters = append(letters, value)
+		}
+		// fmt.Println(string(v))
+	}
+	fmt.Println("uniq letters", len(letters))
+	return len(letters)
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
