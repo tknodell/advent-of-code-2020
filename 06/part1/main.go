7e4dac1cccbe22ba92a369ef587bb25e152524d1
tknodell <tknodell@users.noreply.github.com>
2020-12-07 14:12:21 -0700

    part 2 is tricky

--- 06/part1/main.go
+++ 06/part1/main.go
@@ -25,14 +25,35 @@ func main() {
 	res, _ := readLines("input.txt")
 	var group string
 	var countSum int
+	var groupIndex int
+	var validGroup bool = true
 	for _, v := range res {
 		if v != "" {
-			// fmt.Println("adding to group", v)
-			group += v
+			if groupIndex == 0 {
+				fmt.Println("adding first value", v)
+				group += v
+				groupIndex++
+				fmt.Println(groupIndex, v)
+			} else {
+				if !stringContains(group, v) {
+					fmt.Printf("%s doesnt contain %v skipping \n", group, v)
+					validGroup = false
+				} else {
+					if stringContains(v, group) {
+						fmt.Println("adding to group", v)
+						group += v
+					}
+				}
+			}
 		} else {
 			fmt.Println("new group; result is", group)
-			countSum += uniqLetters(group)
+			if validGroup {
+				fmt.Println("THIS IS RIGHT")
+				countSum += uniqLetters(group)
+			}
 			group = ""
+			groupIndex = 0
+			validGroup = true
 		}
 		fmt.Println(v)
 	}
@@ -53,6 +74,15 @@ func uniqLetters(s string) int {
 	return len(letters)
 }
 
+func stringContains(s string, e string) bool {
+	for _, a := range s {
+		if string(a) == e {
+			return true
+		}
+	}
+	return false
+}
+
 func contains(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
@@ -61,3 +91,6 @@ func contains(s []string, e string) bool {
 	}
 	return false
 }
+
+// 46 not right
+// 25 not right
