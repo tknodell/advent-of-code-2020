66809ed5e8cb25048a6a731cd5d11cb6a667d2e2
Tyler Knodell <tyler.knodell@workday.com>
2020-12-08 10:50:24 -0700

    start on day 8

---
+++ 08/part1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func main() {
+	lines, _ := readLines("test.txt")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+}
