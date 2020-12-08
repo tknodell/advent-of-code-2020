22387c8b32facb84ea0f4b97a572c840270e3ed2
Tyler Knodell <tyler.knodell@workday.com>
2020-12-08 12:39:48 -0700

    day 8 part1

--- 08/part1/main.go
+++ 08/part1/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func readLines(path string) ([]string, error) {
@@ -22,8 +24,38 @@ func readLines(path string) ([]string, error) {
 }
 
 func main() {
-	lines, _ := readLines("test.txt")
-	for _, line := range lines {
-		fmt.Println(line)
+	lines, _ := readLines("input.txt")
+
+	var indexSeen []int
+	var index, accumulator int
+
+	for !contains(indexSeen, index) {
+		indexSeen = append(indexSeen, index)
+		instruction := strings.Split(lines[index], " ")
+
+		num, _ := strconv.Atoi(instruction[1])
+
+		switch instruction[0] {
+		case "acc":
+			fmt.Println("Adding", num)
+			accumulator += num
+		case "jmp":
+			fmt.Println("Jumping", num)
+			index += num - 1
+		}
+
+		index++
 	}
+
+	fmt.Println()
+	fmt.Println("Answer is:", accumulator)
+}
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
