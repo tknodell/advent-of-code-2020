436b2ae70d2b5d7aebe4642ca6ca6fbc79df3dfb
tknodell <tknodell@users.noreply.github.com>
2020-12-07 17:55:41 -0700

    Works!!

--- 06/part2/main.go
+++ 06/part2/main.go
@@ -9,32 +9,44 @@ import (
 func main() {
 
 	var numInGroup int
-
-	letters := popAlphabet()
-	fmt.Println(letters)
+	var letterCount map[string]int = popAlphabet()
+	var totalScore int
 
 	lines, _ := readLines("input.txt")
 	for _, v := range lines {
 
 		if v == "" {
+			fmt.Println(letterCount)
 			fmt.Println("NEW GROUP, previous had", numInGroup)
+			score := getValidLetters(letterCount, numInGroup)
+			fmt.Println("SCORE IS", score)
+			totalScore += score
 			numInGroup = 0
+			letterCount = popAlphabet()
+		} else {
+			numInGroup++
 		}
 
-		numInGroup++
-
 		// loop thru letters
-		letterCount := popAlphabet()
-
 		for _, l := range v {
 			letterString := string(l)
 			letterCount[letterString] += 1
-			fmt.Println(letterString)
 		}
-		fmt.Println(letterCount)
-		fmt.Println("NEW LINE", v)
+
+		fmt.Println(v)
+	}
+	fmt.Println("TOTAL SCORE IS ", totalScore)
+}
+
+func getValidLetters(m map[string]int, count int) int {
+	var sum int
+	for _, v := range m {
+		if v == count {
+			sum++
+		}
 	}
 
+	return sum
 }
 
 func popAlphabet() map[string]int {
