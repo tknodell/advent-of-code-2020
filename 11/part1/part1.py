8fa2f93b4f0959bb82d74c8db2fb8ede0f3e60a1
Tyler Knodell <tyler.knodell@workday.com>
2020-12-11 13:58:03 -0700

    add some directions

--- 11/part1/part1.py
+++ 11/part1/part1.py
@@ -21,28 +21,70 @@ def split(line):
 
 
 def countAdajcentSeats(map, currentSeatX, currentSeatY):
-    print(map[currentSeatX][currentSeatY])
+    # print(currentSeatX,currentSeatY)
+    # print(map[currentSeatX][currentSeatY])
     rows = len(map)
-    seats = len(split(map[currentSeatX]))
+    seats = len(split(map[currentSeatX-1]))
 
     print("num of rows",rows)
-    print("num of seats",seats)
-
-    # cols = len(map[currentSeatX].split())
-    # print("num of cols",cols)
+    print("num of seats in row #",currentSeatY,seats)
 
     # up
+    if currentSeatX==0:
+        print("on top row, skipping up")
+    else:
+        print(map[currentSeatX-1][currentSeatY])
+
     # down
+    if currentSeatY==rows:
+        print("on bottom row, skipping down")
+    else:
+        print(map[currentSeatX+1][currentSeatY])
+
     # left
+    if currentSeatX == 0:
+        print("on leftmost row, skipping")
+    else:
+        print(map[currentSeatX][currentSeatY-1])
+
     # right
+    if currentSeatX == seats:
+        print("on rightmost row, skipping")
+    else:
+        print(map[currentSeatX][currentSeatY+1])
+
 
     # up-left
+    if currentSeatX==0:
+        print("on top row, skipping")
+    else:
+        print(map[currentSeatX-1][currentSeatY-1])
+
     # up-right
+    if currentSeatX==0:
+        print("on top row, skipping")
+    else:
+        print(map[currentSeatX-1][currentSeatY+1])
+
     # down-left
+    if currentSeatX+1==rows:
+        print("on bottom row, skipping")
+    else:
+        print(map[currentSeatX+1][currentSeatY-1])
+
     # down-right
+    if currentSeatX+1==rows:
+        print("on bottom row, skipping")
+    else:
+        print(map[currentSeatX+1][currentSeatY+1])
+
+
 
 for seat in seat_map:
     print(seat)
 
 print("---")
-countAdajcentSeats(seat_map,0,0)
+# countAdajcentSeats(seat_map,0,0)
+# countAdajcentSeats(seat_map,0,10)
+# countAdajcentSeats(seat_map,10,0)
+countAdajcentSeats(seat_map,10,10)
