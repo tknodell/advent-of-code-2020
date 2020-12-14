43e4f75a88f0a111cc673466fd9d8561a9502c6c
Tyler Knodell <tyler.knodell@workday.com>
2020-12-14 10:35:37 -0700

    better solution

--- 11/part1/part1.py
+++ 11/part1/part1.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 seat_map = []
 
 with open('test.txt') as fp:
@@ -7,87 +9,35 @@ with open('test.txt') as fp:
      seat_map.append(line[:-1])
      line = fp.readline()
 
-def traverse(dx, dy):
-  x, y, tree_count = 0, 0, 0
-  while y < len(seat_map):
-    if seat_map[y][x % len(seat_map[0])] == '#':
-      tree_count = tree_count + 1
-    x = x + dx 
-    y = y + dy
-  return tree_count
+def countAdajcentSeats(grid, x, y):
+    neighbors = []
+    rows, cols = len(grid[0]), len(grid)
 
-def split(line): 
-    return [char for char in line]  
+    if x > 0: # not top row
+        neighbors.append(grid[x-1][y]) # up
+        if y > 0:
+            neighbors.append(grid[x-1][y-1]) # up left
+        if y < rows - 1:
+            neighbors.append(grid[x-1][y+1]) # up right
+    if x < cols-1: # not bottom row
+        neighbors.append(grid[x+1][y]) # down
+        if y > 0:
+            neighbors.append(grid[x+1][y-1]) # down left
+        if y < rows - 1:
+            neighbors.append(grid[x+1][y+1]) # down right
+    if y > 0:
+        neighbors.append(grid[x][y-1]) # left
+    if y < rows - 1:
+        neighbors.append(grid[x][y+1]) # right
 
-
-def countAdajcentSeats(map, currentSeatX, currentSeatY):
-    # print(currentSeatX,currentSeatY)
-    # print(map[currentSeatX][currentSeatY])
-    rows = len(map)
-    seats = len(split(map[currentSeatX-1]))
-
-    print("num of rows",rows)
-    print("num of seats in row #",currentSeatY,seats)
-
-    currentSeatX=currentSeatX-1
-    currentSeatY=currentSeatY-1
-
-    # up
-    if currentSeatX-1<=0:
-        print("on top row, skipping up")
-    else:
-        print(map[currentSeatX-1][currentSeatY])
-
-    # down
-    if currentSeatY-1<=rows:
-        print("on bottom row, skipping down")
-    else:
-        print(map[currentSeatX+1][currentSeatY])
-
-    # left
-    if currentSeatY-1 <= 0:
-        print("on leftmost row, skipping")
-    else:
-        print(map[currentSeatX][currentSeatY-1])
-
-    # right
-    if currentSeatY-1 <= seats:
-        print("on rightmost row, skipping")
-    else:
-        print(map[currentSeatX][currentSeatY+1])
-
-
-    # up-left
-    if currentSeatX==0:
-        print("on top row, skipping")
-    else:
-        print(map[currentSeatX-1][currentSeatY-1])
-
-    # up-right
-    if currentSeatX==0:
-        print("on top row, skipping")
-    else:
-        print(map[currentSeatX-1][currentSeatY+1])
-
-    # down-left
-    if currentSeatX+1==rows:
-        print("on bottom row, skipping")
-    else:
-        print(map[currentSeatX+1][currentSeatY-1])
-
-    # down-right
-    if currentSeatX+1==rows:
-        print("on bottom row, skipping")
-    else:
-        print(map[currentSeatX+1][currentSeatY+1])
-
-
+    print(neighbors)
+    print(Counter(neighbors))
 
 for seat in seat_map:
     print(seat)
 
 print("---")
-# countAdajcentSeats(seat_map,0,0)
-# countAdajcentSeats(seat_map,0,10)
-# countAdajcentSeats(seat_map,10,0)
-countAdajcentSeats(seat_map,10,10)
+countAdajcentSeats(seat_map,0,0)
+countAdajcentSeats(seat_map,0,9)
+countAdajcentSeats(seat_map,9,0)
+countAdajcentSeats(seat_map,9,9)
