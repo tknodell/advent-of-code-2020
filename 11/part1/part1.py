e93efca1e505b40cc0e88c05926c089b7f37a0d9
Tyler Knodell <tyler.knodell@workday.com>
2020-12-14 11:20:06 -0700

    works on test data

--- 11/part1/part1.py
+++ 11/part1/part1.py
@@ -35,10 +35,22 @@ def countAdajcentSeats(grid, x, y):
     # print(Counter(neighbors))
     return Counter(neighbors)
 
-# copy seat map so we can apply simultaneously
-seat_map_original = copy.deepcopy(seat_map)
+def countOccupiedSeats(grid):
+    counter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col]=="#":
+                counter = counter+1
+    return counter
 
-for i=0;i<1;i++:
+maxCount = 0
+
+while maxCount < 10:
+    seatsChange = False
+
+    # copy seat map so we can apply simultaneously
+    seat_map_original = copy.deepcopy(seat_map)
+
     for row in range(len(seat_map_original)):
         for col in range(len(seat_map_original[0])):
             currentSeat = seat_map_original[row][col]
@@ -48,9 +60,19 @@ for i=0;i<1;i++:
             if currentSeat=="L" and neighborCount["#"]==0:
                 # print("occuping", row,col)
                 seat_map[row][col]= "#"
+                seatsChange = True
 
-            # if currentSeat=="#" and neighborCount["#"]>=4:
-            #     print("clearing", row,col)
+            if currentSeat=="#" and neighborCount["#"]>=4:
+                # print("clearing", row,col)
+                seat_map[row][col]= "L"
+                seatsChange = True
+    maxCount= maxCount+1
 
-for seat in seat_map:
-    print(seat)
+    print("---")
+    for seat in seat_map:
+        print(seat)
+
+    if seatsChange == False:
+        print("No seats changed after this many rounds",maxCount)
+        print(countOccupiedSeats(seat_map))
+        exit(0)
