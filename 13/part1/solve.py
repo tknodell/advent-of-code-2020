45552aa15806eae0ed9bf3fe0527c97757f53ea9
tknodell <tknodell@users.noreply.github.com>
2020-12-14 21:02:39 -0700

    wip

--- 13/part1/solve.py
+++ 13/part1/solve.py
@@ -7,16 +7,28 @@ with open('test.txt') as fp:
         lines.append(line[:-1])
         line = fp.readline()
  
-departTime = lines[0]
+departTime = int(lines[0])
 schedules = lines[1].split(",")
 validSched = []
 
 for s in schedules:
     if s != "x":
-        validSched.append(s)
+        validSched.append(int(s))
 
+startTime = 0
+maxTime = int(departTime)+10
+i=0
 
-
-
-print(departTime)
 print(validSched)
+for i in range(departTime-10,maxTime+10):
+    print(i)
+    minsAfter=i%60
+ 
+    for sched in validSched:
+        if i%sched==0:
+            print("bus {} departing {} after the hour, have to wait {} minutes".format(sched,i%60,i-departTime))
+
+    # print(i,minsAfter)
+    # if minsAfter in validSched:
+    #     print("bus {} departing at {}").format(minsAfter,i)
+    i = i+1
