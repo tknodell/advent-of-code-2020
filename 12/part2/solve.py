5ae62b249450702cc6311110aec105bfb4602931
Tyler Knodell <tyler.knodell@workday.com>
2020-12-14 17:31:10 -0700

    working

--- 12/part2/solve.py
+++ 12/part2/solve.py
@@ -2,7 +2,7 @@ import copy
 
 lines = []
 
-with open('test.txt') as fp:
+with open('input.txt') as fp:
     line = fp.readline()
     map_row = []
     while line:
@@ -11,24 +11,19 @@ with open('test.txt') as fp:
 
 
 directions = {
-    "north":0,
-    "east":90,
-    "south":180,
-    "west":270,
+    "north": 0,
+    "east": 90,
+    "south": 180,
+    "west": 270,
 }
+
 
 def rotateWaypoint(waypoint, angle):
     waypointResult = copy.deepcopy(waypoint)
 
-    if angle>0:
-        dir="clockwise"
-    else:
-        dir="counterclockwise"
-
-    numRotations = angle/90
-    for _ in range(numRotations):
-        if dir=="clockwise":
-            print("rotating 90 clockwise")
+    if angle > 0:
+        print("rotating clockwise", angle)
+        if angle == 90:
             # north to east
             waypointResult["east"] = waypoint["north"]
             # east to south
@@ -37,18 +32,57 @@ def rotateWaypoint(waypoint, angle):
             waypointResult["west"] = waypoint["south"]
             # west to north
             waypointResult["north"] = waypoint["west"]
-        if dir=="counterclockwise":
-            print("rotating 90 counter-clockwise")
+        if angle == 180:
+            # north to south
+            waypointResult["south"] = waypoint["north"]
+            # east to west
+            waypointResult["west"] = waypoint["east"]
+            # south to north
+            waypointResult["north"] = waypoint["south"]
+            # west to east
+            waypointResult["east"] = waypoint["west"]
+        if angle == 270:
             # north to west
             waypointResult["west"] = waypoint["north"]
-            # west to south
-            waypointResult["south"] = waypoint["west"]
-            # south to east
-            waypointResult["east"] = waypoint["south"]
             # east to north
             waypointResult["north"] = waypoint["east"]
+            # south to east
+            waypointResult["east"] = waypoint["south"]
+            # west to south
+            waypointResult["south"] = waypoint["west"]
+
+    if angle < 0:
+        print("rotating counter-clockwise", angle)
+        if abs(angle) == 90:
+            # north to west
+            waypointResult["west"] = waypoint["north"]
+            # east to north
+            waypointResult["north"] = waypoint["east"]
+            # south to east
+            waypointResult["east"] = waypoint["south"]
+            # west to south
+            waypointResult["south"] = waypoint["west"]
+        if abs(angle) == 180:
+            # north to south
+            waypointResult["south"] = waypoint["north"]
+            # east to west
+            waypointResult["west"] = waypoint["east"]
+            # south to north
+            waypointResult["north"] = waypoint["south"]
+            # west to east
+            waypointResult["east"] = waypoint["west"]
+        if abs(angle) == 270:
+            # north to east
+            waypointResult["east"] = waypoint["north"]
+            # east to south
+            waypointResult["south"] = waypoint["east"]
+            # south to west
+            waypointResult["west"] = waypoint["south"]
+            # west to north
+            waypointResult["north"] = waypoint["west"]
 
     return waypointResult
+
 
 def manhattanDistance(pos):
     x = abs(pos["east"]-pos["west"])
@@ -56,20 +90,21 @@ def manhattanDistance(pos):
 
     return x+y
 
+
 initDirection = "east"
 position = {"north": 0, "east": 0, "south": 0, "west": 0}
 waypoint = {"north": 1, "east": 10, "south": 0, "west": 0}
 
 for line in lines:
-    # print(line)
+    print(line)
     direction = line[:1]
     steps = int(line[1:])
 
     # rotate by angle
     if direction == "R":
-        waypoint = rotateWaypoint(waypoint,steps)
+        waypoint = rotateWaypoint(waypoint, steps)
     if direction == "L":
-        waypoint = rotateWaypoint(waypoint,-steps)
+        waypoint = rotateWaypoint(waypoint, -steps)
 
     # move number of steps
     if direction == "F":
@@ -77,17 +112,20 @@ for line in lines:
             # print(point, waypoint[point], waypoint[point]*steps)
             position[point] += waypoint[point]*steps
     if direction == "N":
-        waypoint["north"]+=steps
+        waypoint["north"] += steps
     if direction == "E":
-        position["east"] *= steps
+        waypoint["east"] += steps
     if direction == "S":
-        position["south"] *= steps
+        waypoint["south"] += steps
     if direction == "W":
-        position["west"] *= steps
+        waypoint["west"] += steps
 
+    print("position", position)
+    print("waypoint:", waypoint)
     print("---")
-    print(line)
-    print("position",position)
-    print("waypoint:",waypoint)
+
 
 print(manhattanDistance(position))
+
+# 33756 too high
+# 25580 not right
