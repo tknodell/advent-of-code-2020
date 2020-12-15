189dde29b6ecf9a5ac0f5f31f7dbd201bad27071
Tyler Knodell <tyler.knodell@workday.com>
2020-12-15 13:24:32 -0700

    switch to golang

---
+++ 15/part1/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// input := []int{0, 3, 6} // test input
+	input := []int{1, 0, 16, 5, 17, 4}
+
+	fmt.Println("Answer after 2020 runs", Play(input, 2020))
+}
+
+func Play(starting []int, maxTurns int) int {
+	lastSpoken := make(map[int]int)
+
+	var previous int
+	speak := starting[0]
+
+	for turn := 0; turn < maxTurns; turn++ {
+		previous = speak
+
+		if turn < len(starting) {
+			speak = starting[turn] // add inital input values
+		} else if _, ok := lastSpoken[speak]; !ok {
+			speak = 0
+		} else {
+			speak = turn - lastSpoken[speak] - 1
+		}
+
+		lastSpoken[previous] = turn - 1
+		fmt.Println(speak)
+	}
+
+	return speak
+}
--- 15/part1/solve.py
+++
@@ -1,9 +0,0 @@
-import copy
-
-
-testData=[0,3,6]
-inputData=[1,0,16,5,17,4]
-
-
-for v in testData:
-    print(v)
