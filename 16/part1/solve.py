cd72ad173e347fa4c8f0358dace18836d092e282
Tyler Knodell <tyler.knodell@workday.com>
2020-12-17 12:17:59 -0700

    nums into a list

--- 16/part1/solve.py
+++ 16/part1/solve.py
@@ -12,14 +12,19 @@ rules = parts[0]
 myTicket = parts[1]
 nearbyTickets = parts[2]
 
+validNums=[]
+
 for rule in rules.split("\n"):
     ruleName = rule.split(":")[0]
     firstRange = rule.split("or")[0].split(" ")[1]
     secondRange = rule.split("or")[1].split(" ")[1]
 
-    print(firstRange)
-    print(secondRange)
+    # print(ruleName,firstRange, secondRange)
+    for v in (firstRange,secondRange):
+        nums = v.split('-')
+        for num in range(int(nums[0]),int(nums[1])+1):
+            if num not in validNums:
+                validNums.append(num)
+    validNums.sort()
 
-# print(rules)
-# print(myTicket)
-# print(nearbyTickets)
+print(validNums)
